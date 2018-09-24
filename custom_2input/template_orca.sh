9528041d178607a59521a2fe5450e5423b6893ad
Tizian <35625593+tizianmueller@users.noreply.github.com>
2018-09-24 14:52:07 +0200

    basic implementation of "pattern"

---
+++ custom_2input/template_orca.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# This is an example file for the creation of input templates.
+# every line starting with an '#' will not be read.
+# all Variables used in 2input can be specified here:
+
+### NECESSARY 
+
+#program=gaussian
+# or 
+#program=orca
+
+
+
+### OPTIONAL
+
+#multi=1
+#charge=0
+#prefix=""
+#route=""
+#filetype="dat"
+#block=""
+#proc=6
+#mem=2000
+#add_comment=""
+
+
