ce80b1fcf2ca7b9e29fee08998e7706a487efde6
tizanmueller <tizian@DESKTOP-RP7LCPV.localdomain>
2020-07-03 08:14:13 +0200

    initial commit of new templates (unfinished)

---
+++ custom_2input/low_smd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Final optimization at PBE0 def2-TZVP
+
+### NECESSARY 
+
+#program=gaussian
+# or 
+program=orca
+route="! PBEh-3c CPCM TIGHTSCF  Grid4 NoFinalGrid"
+filetype="inp"
+block="%cpcm  \n\tsmd true\n\t smdsolvent \"Toluene\" \n end"
+
+proc=4
+mem=2000
+
+
+### OPTIONAL
+
+#multi=1
+#charge=0
+prefix="SMD_"
+add_comment="PBEh-3c SMD-SP for low level scheme"
